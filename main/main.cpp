e90674ad301a214b14151df863041abe3a0092a2
redstoneee <dc_redstone@outlook.com>
2024-07-20 18:19:31 +0800

    remove some shit

--- main/main.cpp
+++ main/main.cpp
@@ -33,23 +33,17 @@ extern "C" void app_main()
     initArduino();
 
     static auto colPins = {PIN_COL1, PIN_COL2, PIN_COL3, PIN_COL4, PIN_COL5, PIN_COL6};
-
     for (auto p : colPins)
         pinMode(p, OUTPUT);
 
-#if CONFIG_PM_ENABLE
-    // Configure dynamic frequency scaling:
-    // maximum and minimum frequencies are set in sdkconfig,
-    // automatic light sleep is enabled if tickless idle support is enabled.
+    // Configure dynamic frequency scaling
+    // automatic light sleep is enabled
     esp_pm_config_t pm_config = {
         .max_freq_mhz = 48,
         .min_freq_mhz = 8,
-#if CONFIG_FREERTOS_USE_TICKLESS_IDLE
-        .light_sleep_enable = true
-#endif
+        .light_sleep_enable = true,
     };
     ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
-#endif // CONFIG_PM_ENABLE
 
     blue::init();
 
